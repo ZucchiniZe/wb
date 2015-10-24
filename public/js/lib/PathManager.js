8fe97ed8976e47878d41fcc5b9f8a9fef6029b9f
Shahar Zimmerman <shahar.zimmerman.usa@gmail.com>
2015-10-24 15:18:10 -0700

    asd

--- public/js/lib/PathManager.js
+++ public/js/lib/PathManager.js
@@ -4,9 +4,11 @@ var wb;
     wb.PathManager = (function() {
         var pm = function() {
             this.path = false;
+            this.prevPaths = [];
         };
         pm.prototype.setPath = function(path) { this.path = path; };
         pm.prototype.getPath = function() { return this.path; };
+        pm.prototype.pushPath = function(path) { this.prevPaths.push(path); };
         return pm;
     })();
 
