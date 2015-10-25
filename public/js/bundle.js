d0f9aff8ed3111d02d2cfbea593a51e565da2558
Shahar Zimmerman <shahar.zimmerman.usa@gmail.com>
2015-10-25 13:40:03 -0700

    asd

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -80,6 +80,9 @@
 
 	    console.log('pathname:', pathname, 'path:', path);
 	    socket.emit('health:alive', path);
+
+	    // for angular to use in $window service
+	    window.wbcanvas = canvas;
 	};
 
 /***/ },
@@ -87,6 +90,10 @@
 /***/ function(module, exports) {
 
 	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	    value: true
+	});
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
@@ -136,13 +143,19 @@
 	    return PathManager;
 	})();
 
-	module.exports = PathManager;
+	exports['default'] = PathManager;
+	;
+	module.exports = exports['default'];
 
 /***/ },
 /* 2 */
 /***/ function(module, exports) {
 
 	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
@@ -210,13 +223,16 @@
 	            if (d1.point && d2.point) {
 	                return d1.point.equals(d2.point);
 	            }
+	            return false;
 	        }
 	    }]);
 
 	    return SocketEmitter;
 	})();
 
-	module.exports = SocketEmitter;
+	exports["default"] = SocketEmitter;
+	;
+	module.exports = exports["default"];
 
 /***/ },
 /* 3 */
@@ -267,7 +283,7 @@
 	            _Users2['default'].init(angular, socket);
 	            _Tool2['default'].init(angular, socket);
 
-	            angular.module('wb:gui', ['wb:users', 'wb:tool', 'wb:service:pathmanager']).controller('gui', ['$scope', 'UsersService', 'ToolService', 'PathManagerService', function ($scope, UsersService, Tool, PathManager) {
+	            angular.module('wb:gui', ['wb:users', 'wb:tool', 'wb:service:pathmanager']).controller('gui', ['$scope', 'UsersService', 'ToolService', 'PathManagerService', '$window', function ($scope, UsersService, Tool, PathManager, $window) {
 
 	                var clientPath = PathManager.getClient(),
 	                    socketPath = PathManager.getSocket();
@@ -304,6 +320,10 @@
 	                    userColor += '0';
 	                }
 	                userColor += randomHex;
+
+	                $scope.clear = function () {
+	                    paper.project.clear();
+	                };
 
 	                $scope.selectTool = function (type) {
 	                    $scope.selectedTool = type;
@@ -432,6 +452,7 @@
 	})();
 
 	exports['default'] = Users;
+	;
 	module.exports = exports['default'];
 
 /***/ },
@@ -585,6 +606,8 @@
 	                                tools.eraser = eraser;
 	                                return eraser;
 	                        }
+
+	                        return false;
 	                    }
 	                };
 	            }]);
@@ -595,6 +618,7 @@
 	})();
 
 	exports['default'] = Tool;
+	;
 	module.exports = exports['default'];
 
 /***/ },
